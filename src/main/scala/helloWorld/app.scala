1e757fbf62ba7d8fca61a7398fa65650c91daa5d
vermosen <vermosen.jm@free.fr>
2017-06-18 17:06:42 -0400

    initial commit

---
+++ src/main/scala/helloWorld/app.scala
@@ -0,0 +1,26 @@
+package helloWorld
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.log4j.Logger
+
+object app 
+{
+    // a simple project to test spark initialization
+    def main(arg: Array[String]) 
+    {
+        val conf = new SparkConf()
+            conf.setAppName("helloWorld")
+                .setMaster("local")
+      
+        val sc = new SparkContext(conf)
+            sc.setLogLevel("WARN")
+        
+        // do stuff
+        println("Hello, world!")
+        
+        // terminate spark context
+        sc.stop()
+    }
+}
